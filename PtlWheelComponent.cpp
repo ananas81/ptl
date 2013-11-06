b411525e26143eef51cc8c9c765a245dd6a70fe6
Jacek Anaszewski <anjack@poczta.onet.pl>
2013-11-06 22:56:34 +0100

    almost working wheel on rack

--- PtlWheelComponent.cpp
+++ PtlWheelComponent.cpp
@@ -37,19 +37,75 @@ WheelBodyComponent::WheelBodyComponent(Ogre::SceneManager *aSceneMgr,
 
 	btRigidBody *wheelBody = static_cast<btRigidBody*>(mWheel->getCollisionObject());
 	btRigidBody *rackBody = static_cast<btRigidBody*>(mRack->getCollisionObject());
+	//rackBody->setLinearFactor(btVector3(0, 0, 0.5));
+	//rackBody->setAngularFactor(btVector3(0, 0, 0.5));
+	//rackBody->setActivationState(DISABLE_DEACTIVATION);
 
-	mWheelHinge = new btHingeConstraint(*wheelBody, btVector3(0,0,0), btVector3(0,0,1), true);
+	btTransform rackBottom = btTransform::getIdentity();
+	rackBottom.setOrigin(btVector3(0., -170.0, 0.));
+	//rackBottom.setRotation(btQuaternion(0 , sqrt(0.5), 0, sqrt(0.5)));
+	btGeneric6DofConstraint *dofConstr = new btGeneric6DofConstraint(*rackBody, rackBottom, true);
+
+        btRotationalLimitMotor *dofRotMotor;
+
+	dofConstr->getTranslationalLimitMotor()->m_enableMotor[0] = true;
+	dofConstr->setLinearUpperLimit(btVector3(10.0, 0.0, 0.0));
+	dofConstr->setLinearLowerLimit(btVector3(-10.0, 0.0, 0.0));
+	dofConstr->setAngularUpperLimit(btVector3(0.0, 0.0, 0.0));
+	dofConstr->setAngularLowerLimit(btVector3(0.0, 0.0, 0.0));
+        for (int i = 0; i < 3; ++i)
+	{
+		dofRotMotor = dofConstr->getRotationalLimitMotor(i);
+		dofRotMotor->m_enableMotor = true;
+		dofRotMotor->m_normalCFM = 0.0;
+		dofRotMotor->m_stopCFM = 0.0;
+		dofRotMotor->m_stopERP = 0.0;
+		dofRotMotor->m_maxLimitForce = 100.0;
+		dofRotMotor->m_maxMotorForce = 100.0;
+//	      dofRotMotor->m_hiLimit = 0.2;
+//	      dofRotMotor->m_loLimit = 0.2;
+		dofRotMotor->m_limitSoftness = 0.0;
+	}
+
+/*
+w	x	y	z	Description
+1	0	0	0	Identity quaternion, no rotation
+0	1	0	0	180° turn around X axis
+0	0	1	0	180° turn around Y axis
+0	0	0	1	180° turn around Z axis
+sqrt(0.5)	sqrt(0.5)	0	0	90° rotation around X axis
+sqrt(0.5)	0	sqrt(0.5)	0	90° rotation around Y axis
+sqrt(0.5)	0	0	sqrt(0.5)	90° rotation around Z axis
+sqrt(0.5)	-sqrt(0.5)	0	0	-90° rotation around X axis
+sqrt(0.5)	0	-sqrt(0.5)	0	-90° rotation around Y axis
+sqrt(0.5)	0	0	-sqrt(0.5)	-90° rotation around Z axis*/
+
+//	mWheelHinge = new btHingeConstraint(*wheelBody, btVector3(0,0,0), btVector3(0,0,1), true);
+//	mWheelHinge = new btHingeConstraint (btRigidBody &rbA, btRigidBody &rbB, const btVector3 &pivotInA, const btVector3 &pivotInB, const btVector3 &axisInA, const btVector3 &axisInB, bool useReferenceFrameA=false)
+
+	//mWheelHinge = new btHingeConstraint(*wheelBody, *rackBody, btVector3(0,0,0), btVector3(0,0,0), btVector3(0,0,1), btVector3(0,0,1), true);
+	btTransform wheelFrame = btTransform::getIdentity();
+	wheelFrame.setOrigin(btVector3(0., 0., -3.64));
+	//wheelFrame.setRotation(btQuaternion(0, 0, 1, 0));
+	btTransform rackFrame = btTransform::getIdentity();
+//	rackFrame.setOrigin(btVector3(150.0, 0., 0.));
+	rackFrame.setOrigin(btVector3(150.0, 0., 0.));
+	rackFrame.setRotation(btQuaternion(0 , sqrt(0.5), 0, sqrt(0.5)));
+	
+	//mWheelHinge = new btHingeConstraint(*wheelBody, *rackBody, wheelFrame, rackFrame, true);
+	mWheelHinge = new btHingeConstraint(*wheelBody, *rackBody, btVector3(0, 0, -3.64), btVector3(150.0, 0, 0), btVector3(0, 0, 1), btVector3(1, 0, 0), true);
 
 	wheelBody->setFriction(1);
 	wheelBody->setDamping(0.1f,0.1f);
 	wheelBody->setFlags(0);
-	wheelBody->setLinearFactor(btVector3(0, 0, 0.5));
-	wheelBody->setAngularFactor(btVector3(0, 0, 0.5));
+//	wheelBody->setLinearFactor(btVector3(0, 0, 0.5));
+//	wheelBody->setAngularFactor(btVector3(0, 0, 0.5));
 	wheelBody->setActivationState(DISABLE_DEACTIVATION);
 
-	mWorld->addConstraint(mWheelHinge);              
 	mWorld->addRigidBody(wheelBody);
 	mWorld->addRigidBody(rackBody);
+	mWorld->addConstraint(mWheelHinge);              
+	mWorld->addConstraint(dofConstr);
 
 	btTransform frameInA;
 
