9f76c8a0e621065e692af0240849b11aa57d72cb
Jacek Anaszewski <anjack@poczta.onet.pl>
2013-11-27 21:31:06 +0100

    clean up wheel component

--- PtlWheelComponent.cpp
+++ PtlWheelComponent.cpp
@@ -23,16 +23,6 @@ WheelBodyComponent::WheelBodyComponent(Ogre::SceneManager *aSceneMgr,
 						  Ogre::Vector3(0, 0, 0),
 						  0.,
 						  0.);
-//	mRack = new Ptl::OgrePhysicalBody(mSceneMgr,
-//						  "Rack",
-//						  "Sphere.mesh",
-//						  Ogre::Vector3(25, 10, -150),
-//						  mOrient,
-//						  new Ptl::BtOgreShapeDispatcher(NULL, Ptl::BtOgreShapeDispatcher::CONVEX_HULL),
-//						  1.0,
-//						  Ogre::Vector3(0, 0, 0),
-//						  10.0,
-//						  1.0);
 
 	mWheel = new Ptl::OgrePhysicalBody(mSceneMgr,
 						  "Flywheel",
@@ -48,52 +38,35 @@ WheelBodyComponent::WheelBodyComponent(Ogre::SceneManager *aSceneMgr,
 	btRigidBody *wheelBody = static_cast<btRigidBody*>(mWheel->getCollisionObject());
 	btRigidBody *rackBody = static_cast<btRigidBody*>(mRack->getCollisionObject());
 	rackBody->setLinearFactor(btVector3(1, 1, 1));
-	//rackBody->setAngularFactor(btVector3(0, 0, 0.5));
-	//rackBody->setActivationState(DISABLE_DEACTIVATION);
 
 	btTransform rackBottom = btTransform::getIdentity();
-	//rackBottom.setOrigin(btVector3(25., 10., -150.));
 	rackBottom.setOrigin(btVector3(0., mPos.y-170.0, 0.));
-	//rackBottom.setRotation(btQuaternion(0 , sqrt(0.5), 0, sqrt(0.5)));
 	btGeneric6DofConstraint *dofConstr = new btGeneric6DofConstraint(*rackBody, rackBottom, true);
 	rackConstr = dofConstr;
 
         btRotationalLimitMotor *dofRotMotor;
 
-	//dofConstr->setLinearUpperLimit(btVector3(0.0, 0.0, 0.0));
-	//dofConstr->setLinearLowerLimit(btVector3(0.0, 0.0, 0.0));
 	dofConstr->setLinearUpperLimit(btVector3(0., 0.0, 100.0));
 	dofConstr->setLinearLowerLimit(btVector3(0., 0.0, -100.0));
 	dofConstr->setAngularUpperLimit(btVector3(0, 0, 0));
 	dofConstr->setAngularLowerLimit(btVector3(0, 0, 0));
 
-	//mWheelHinge = new btHingeConstraint(*wheelBody, *rackBody, btVector3(0,0,0), btVector3(0,0,0), btVector3(0,0,1), btVector3(0,0,1), true);
-	btTransform wheelFrame = btTransform::getIdentity();
-	wheelFrame.setOrigin(btVector3(0., 0., -3.64));
-	//wheelFrame.setRotation(btQuaternion(0, 0, 1, 0));
-	btTransform rackFrame = btTransform::getIdentity();
-//	rackFrame.setOrigin(btVector3(150.0, 0., 0.));
-	rackFrame.setOrigin(btVector3(150.0, 0., 0.));
-	rackFrame.setRotation(btQuaternion(0 , sqrt(0.5), 0, sqrt(0.5)));
-	
-	//mWheelHinge = new btHingeConstraint(*wheelBody, *rackBody, wheelFrame, rackFrame, true);
-	mWheelHinge = new btHingeConstraint(*wheelBody, *rackBody, btVector3(0, 0, -3.64), btVector3(150.0, 0, 0), btVector3(0, 0, 1), btVector3(1, 0, 0), true);
+	mWheelHinge = new btHingeConstraint(*wheelBody,
+					    *rackBody,
+					    btVector3(0, 0, -3.64),
+					    btVector3(150.0, 0, 0),
+					    btVector3(0, 0, 1),
+					    btVector3(1, 0, 0), true);
 
 	wheelBody->setFriction(1);
 	wheelBody->setDamping(0.1f,0.1f);
 	wheelBody->setFlags(0);
-//	wheelBody->setLinearFactor(btVector3(0, 0, 0.5));
-//	wheelBody->setAngularFactor(btVector3(0, 0, 0.5));
 	wheelBody->setActivationState(DISABLE_DEACTIVATION);
 
 	mWorld->addRigidBody(wheelBody);
 	mWorld->addRigidBody(rackBody);
 	mWorld->addConstraint(mWheelHinge);              
 	mWorld->addConstraint(dofConstr);
-
-//	rackBody->setLinearVelocity(btVector3(5, 0, 0));
-//	rackBody->setLinearFactor(btVector3(1, 0, 0));
-//	rackBody->setCcdMotionThreshold(0.5);
 
 	btTransform frameInA;
 
@@ -141,24 +114,8 @@ WheelBodyComponent::WheelBodyComponent(Ogre::SceneManager *aSceneMgr,
 
 	mChildComponents[3]->attachTo(wheelBody, frameInA);
 
-	//btTransform rackC = btTransform::getIdentity();
-	//rackC.setOrigin(btVector3(-100., 0., 100.));
-	//rackC.setRotation(btQuaternion(0, sqrt(0.5), 0, sqrt(0.5)));
-
-	//dofConstr->getFrameOffsetA().setOrigin(btVector3(-100., 0., 100.));
 	rackBody->setActivationState(DISABLE_DEACTIVATION);
-	//dofConstr->getFrameOffsetA().setOrigin(btVector3(mPos.x - 150, mPos.y, mPos.z + 100));
 	dofConstr->getFrameOffsetA().setRotation(btQuaternion(0, sqrt(0.5), 0, sqrt(0.5)));
-
-//	rackBody->forceActivationState(ACTIVE_TAG);
-//	rackBody->setDeactivationTime( 0.f );
-/*
-	removeFromWorld();
-	rackBody->setWorldTransform(rackC);
-	addToWorld();
-*/
-//	btGeneric6DofConstraint *_dofConstr = new btGeneric6DofConstraint(*rackBody, rackC, true);
-//	mWorld->addConstraint(_dofConstr);
 }
 
 WheelBodyComponent::~WheelBodyComponent()
